8990969fdefcd82e40ff0d8128f42cea77d06453
mlorber <87738509+mlorberdev@users.noreply.github.com>
2022-12-13 12:23:42 -0500

    v1

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,9 @@
 !(function () {
-  
+
   // SCROLL
   void function scroll() {
     let ll = 0;
-    document.getElementById("wrap").addEventListener("wheel", (e) => {
+    window.addEventListener("wheel", (e) => {
       if (ll === 0 && e.deltaY < 0) return;
       // if (ll > 5 * innerWidth) return;
       e.deltaY < 0 ? ll += 15 : ll -= 15;
@@ -17,9 +17,6 @@
   void function bg() {
 
     const rn = (n) => { return Math.floor(Math.random() * n); }
-    const clr = ["#77777733", "#44444455"];
-    const sym = "123456789+-*&^%$#×÷⊕∈∵∮∬∇δΣ⇔⇒¬";
-    const r = 100;
     const tau = Math.PI * 2;
     const c = document.getElementById("c");
     const c2 = document.getElementById("c2");
@@ -27,28 +24,25 @@
     const cx2 = c2.getContext("2d");
     const w = 5 * innerWidth;
     const h = innerHeight;
+    const r = 100;
     c.width = c2.width = w;
     c.height = c2.height = h;
 
     void function jt() {
-      mk(cx, clr[0]);
-      mk(cx2, clr[1]);
+      mk(cx);
+      mk(cx2);
     }();
 
-    function mk(cxt, clr) {
-      cxt.fillStyle = clr;
-      for (let i = 0; i < 50; i++) {
+    function mk(cxt) {
+      for (let i = 0; i < 200; i++) {
+        cxt.fillStyle = `hsla(${rn(360)},${rn(100)}%,${rn(100)}%,.6)`;
         let x = rn(w);
         let y = rn(h);
         let z = rn(r);
-        if (Math.random() < .7) {
-          cxt.font = `${rn(100) + 96}px Cinzel`;
-          cxt.fillText(sym.charAt(rn(30)), x, y);
-        } else {
-          cxt.beginPath();
-          cxt.arc(x, y, z, 0, tau);
-          cxt.fill();
-        }
+        cxt.beginPath();
+        cxt.arc(x, y, z, 0, tau);
+        cxt.fill();
+
       }
     }
   }();
